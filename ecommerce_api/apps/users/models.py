bd7b84868da3560f26ac051bfa5d008e78992c83
VulturARG <luisbriones@gmail.com>
2022-03-19 11:22:18 -0300

    Changed User str

--- ecommerce_api/apps/users/models.py
+++ ecommerce_api/apps/users/models.py
@@ -87,4 +87,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['email']
 
     def __str__(self):
-        return f'{self.username} {self.email}'
+        return f'{self.username}'
