fc5570676d8a17bd7e7a98052278cd6f5a3de633
VulturARG <luisbriones@gmail.com>
2022-03-20 18:58:36 -0300

    Add product model

--- ecommerce_api/ecommerce_api/urls.py
+++ ecommerce_api/ecommerce_api/urls.py
@@ -8,5 +8,6 @@ from ecommerce_api.settings.base import API_VERSION
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(urllib.parse.urljoin(API_VERSION, 'users/'), include('apps.users.api.urls')),
+    path(urllib.parse.urljoin(API_VERSION, 'products/'), include('apps.products.api.routers')),
 ]
 
