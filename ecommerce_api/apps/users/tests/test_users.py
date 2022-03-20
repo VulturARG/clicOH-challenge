565b431628c49a41168f5c14ba485d30f7437973
VulturARG <luisbriones@gmail.com>
2022-03-20 18:59:52 -0300

    Add test user

---
+++ ecommerce_api/apps/users/tests/test_users.py
@@ -0,0 +1,76 @@
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+from apps.users.models import User
+
+
+class UserTestCase(APITestCase):
+    list_url = reverse("api-user")
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            name="John",
+            last_name="Wick",
+            email="wick@example.com",
+            username="wick",
+            password="jf897453!"
+        )
+
+    def test_bad_path(self):
+        response = self.client.get("/bad_path/", format="json")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_list(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_an_existing_user(self):
+        response = self.client.get("/v1/users/1/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["username"], "wick")
+
+    def test_create_user(self):
+        data = {
+            "name": "Peter",
+            "last_name": "Parker",
+            "email": "peter@example.com",
+            "username": "peter",
+            "password": "jf897453!",
+        }
+
+        response = self.client.post(self.list_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["username"], "peter")
+
+    def test_create_user_with_invalid_email(self):
+        data = {
+            "name": "Peter",
+            "last_name": "Parker",
+            "email": "peter",
+            "username": "peter",
+            "password": "jdf897453!",
+        }
+
+        response = self.client.post(self.list_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_user(self):
+        data = {
+            "name": "Peter",
+            "last_name": "Parker",
+            "email": "peter@pepe.com",
+            "username": "peter",
+            "password": "jhg897453!",
+        }
+
+        response = self.client.put("/v1/users/1/", data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["username"], "peter")
+
+    def test_delete_an_existing_user(self):
+        response = self.client.delete("/v1/users/1/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["message"], "User deleted")
+        
+        
