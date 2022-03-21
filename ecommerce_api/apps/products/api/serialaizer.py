110d687b7cbccad93baf7eb2dfafa6d1fb03cc20
VulturARG <luisbriones@gmail.com>
2022-03-21 14:33:16 -0300

    Using Validate class

--- ecommerce_api/apps/products/api/serialaizer.py
+++ ecommerce_api/apps/products/api/serialaizer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from apps.products.models import Product
+from domain.vatidate.validate import Validate
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -8,13 +9,17 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
 
+    def __init__(self):
+        super().__init__()
+        self.own_validate = Validate()
+
     def validate_price(self, value):
-        if value < 0:
+        if self.own_validate.is_less_than_zero(value):
             raise serializers.ValidationError("Price cannot be negative")
         return value
 
     def validate_stock(self, value):
-        if value < 0:
+        if self.own_validate.is_less_than_zero(value):
             raise serializers.ValidationError("Stock cannot be negative")
         return value
 
