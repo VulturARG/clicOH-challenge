6904a34ca366d20038f3613ad1c6ea612f69d70d
VulturARG <luisbriones@gmail.com>
2022-03-22 10:35:42 -0300

    Added validated methods and test

--- ecommerce_api/domain/vatidate/tests/test_validate.py
+++ ecommerce_api/domain/vatidate/tests/test_validate.py
@@ -7,6 +7,8 @@ class ValidateTestCase(unittest.TestCase):
     def setUp(self):
         self.validate = Validate()
 
+    # validate.is_less_than_zero
+
     def test_equal_to_zero(self):
         actual = self.validate.is_less_than_zero(0)
         self.assertFalse(actual)
@@ -26,3 +28,73 @@ class ValidateTestCase(unittest.TestCase):
     def test_float_less_than_zero(self):
         actual = self.validate.is_less_than_zero(-0.21)
         self.assertTrue(actual)
+
+    # validate.is_string_in_string
+
+    def test_is_string_in_string(self):
+        actual = self.validate.is_string_in_string('cd', 'abcdef')
+        self.assertTrue(actual)
+
+    def test_is_string_not_in_string(self):
+        actual = self.validate.is_string_in_string('yz', 'abcdef')
+        self.assertFalse(actual)
+
+    # validate.is_only_letters
+
+    def test_only_letters(self):
+        actual = self.validate.is_only_letters('abc')
+        self.assertTrue(actual)
+
+    def test_only_letters_empty_string(self):
+        actual = self.validate.is_only_letters('')
+        self.assertFalse(actual)
+
+    def test_only_letters_with_space(self):
+        actual = self.validate.is_only_letters('abc def')
+        self.assertFalse(actual)
+
+    def test_only_letters_with_number(self):
+        actual = self.validate.is_only_letters('abc123')
+        self.assertFalse(actual)
+
+    def test_only_letters_with_special_char(self):
+        actual = self.validate.is_only_letters('abc@def')
+        self.assertFalse(actual)
+
+    # validate.is_upper_letter_in_string
+
+    def test_is_upper_letter_in_string(self):
+        actual = self.validate.is_upper_letter_in_string('abCdef')
+        self.assertTrue(actual)
+
+    def test_not_upper_letter_in_string(self):
+        actual = self.validate.is_upper_letter_in_string('abcdef')
+        self.assertFalse(actual)
+
+    # validate.have_upper_letters_numbers_and_special_characters
+
+    def test_upper_letters_numbers_and_special_characters(self):
+        actual = self.validate.have_upper_letters_numbers_and_special_characters('abZc123@def')
+        self.assertTrue(actual)
+
+    def test_no_upper_letters_numbers_and_special_characters(self):
+        actual = self.validate.have_upper_letters_numbers_and_special_characters('abc123@def')
+        self.assertFalse(actual)
+
+    def test_have_letters_numbers_and_special_characters_with_letters_only(self):
+        actual = self.validate.have_upper_letters_numbers_and_special_characters('abc')
+        self.assertFalse(actual)
+
+    def test_have_letters_numbers_and_special_characters_with_letters_numbers(self):
+        actual = self.validate.have_upper_letters_numbers_and_special_characters('abc125')
+        self.assertFalse(actual)
+
+    def test_have_letters_numbers_and_special_characters_with_letters_special_chars(self):
+        actual = self.validate.have_upper_letters_numbers_and_special_characters('abc_#')
+        self.assertFalse(actual)
+
+    def test_have_letters_numbers_and_special_characters_with_space(self):
+        actual = self.validate.have_upper_letters_numbers_and_special_characters('abc 123@def')
+        self.assertFalse(actual)
+
+
--- ecommerce_api/domain/vatidate/validate.py
+++ ecommerce_api/domain/vatidate/validate.py
@@ -14,12 +14,19 @@ class Validate:
 
         return value.isalpha()
 
-    def have_letters_numbers_and_special_characters(self, value: str) -> bool:
+    def is_string_in_string(self, value: str, string: str) -> bool:
+        """Validate if value is an string inside string."""
+
+        return value in string
+
+    def is_upper_letter_in_string(self, value: str) -> bool:
+        """check if at least one letter of the string is uppercase."""
+
+        return any(char.isupper() for char in value)
+
+    def have_upper_letters_numbers_and_special_characters(self, value: str) -> bool:
         """Validate if value have letters, numbers and special characters."""
 
-        return value.isalnum()
+        return self.is_upper_letter_in_string(value) and not value.isalnum()
 
-    def have_string_in_string(self, value: str, string: str) -> bool:
-        """Validate if value have string in string."""
 
-        return string in value
