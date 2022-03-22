8fedc31a5249835936c3f8eb80042199333dfc90
VulturARG <luisbriones@gmail.com>
2022-03-22 11:29:47 -0300

    Renamed class

--- ecommerce_api/apps/products/api/routers.py
+++ ecommerce_api/apps/products/api/routers.py
@@ -1,8 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from apps.products.api.viewsets import ProductViewSet
+from apps.products.api.viewsets import ProductAPIViewSet
 
 router = DefaultRouter()
-router.register(r'', ProductViewSet, basename='products')
+router.register(r'', ProductAPIViewSet, basename='products')
 urlpatterns = router.urls
 
