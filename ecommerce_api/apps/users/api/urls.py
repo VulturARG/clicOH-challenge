f1f463c6b6a74ea505abf40ddbfbef8d26824871
VulturARG <luisbriones@gmail.com>
2022-03-26 10:36:00 -0300

    Users Modidy

--- ecommerce_api/apps/users/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from apps.users.api.api import UserAPIView, UserDetailAPIView
-
-urlpatterns = [
-    path('', UserAPIView.as_view(), name='api-user'),
-    path('<int:pk>/', UserDetailAPIView.as_view(), name='api-user-detail'),
-]
--- ecommerce_api/apps/users/authentication_mixins.py
+++ ecommerce_api/apps/users/authentication_mixins.py
@@ -1,43 +1,36 @@
+from typing import Optional, Tuple, Any
+
 from rest_framework import status, authentication, exceptions
 from rest_framework.authentication import get_authorization_header
+from rest_framework.request import Request
 
 from apps.users.authentication import ExpiringTokenAuthentication
+from apps.users.models import User
 
 
 class Authentication(authentication.BaseAuthentication):
     user = None
 
-    def get_user(self, request):
-        """
-        Return:
-            * user      : User Instance or
-            * message   : Error Message or
-            * None      : Corrupt Token
-        """
+    def get_user(self, request: Request) -> Optional[User]:
         token = get_authorization_header(request).split()
-        print(token)
-        print(type(token))
+        if not token:
+            return None
 
-        return None
+        try:
+            token = token[1].decode()
+        except IndexError:
+            return None
 
-        # if not token:
-        #     return None
-        #
-        # try:
-        #     token = token[1].decode()
-        # except:
-        #     return None
-        #
-        # token_expire = ExpiringTokenAuthentication()
-        # user = token_expire.authenticate_credentials(token)
-        #
-        # if user is None:
-        #     return None
+        token_expire = ExpiringTokenAuthentication()
+        user = token_expire.authenticate_credentials(token)
+
+        if user is None:
+            return None
 
         self.user = user
         return user
 
-    def authenticate(self, request):
+    def authenticate(self, request: Request) -> Tuple[User, int]:
         self.get_user(request)
         if self.user is None:
             raise exceptions.AuthenticationFailed('credentials not sent.')
