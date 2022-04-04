e1c0ed722526986400f57d6a9f4e828503065e75
VulturARG <luisbriones@gmail.com>
2022-04-04 13:37:18 -0300

    Miscellaneous

--- ecommerce_api/ecommerce_api/settings/base.py
+++ ecommerce_api/ecommerce_api/settings/base.py
@@ -53,14 +53,14 @@ SWAGGER_SETTINGS = {
     'DOC_EXPANSION': 'none'
 }
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     ],
-#     'DEFAULT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.IsAuthenticated',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
